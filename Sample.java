4fce73a236c814d840e873cde401184f4a6420d8
vanithavelayutham <vanitha.v@kggroup.com>
2017-07-13 16:33:44 +0530

    age changes

--- Sample.java
+++ Sample.java
@@ -1,7 +1,7 @@
 class Sample
 {
     public static void main(String[] args) {
-        List<Person> persons=Arrays.asList(new Person("ani",21),new Person("vani",18),new Person("vani",99),new Person("azar",11));
+        List<Person> persons=Arrays.asList(new Person("ani",22),new Person("vani",18),new Person("vani",99),new Person("azar",11));
         //Find Maximum value
         persons.stream().max(Comparator.comparing(Person::getName)).ifPresent(p->System.out.println("sort by alphabet order person max"+p));
 }
